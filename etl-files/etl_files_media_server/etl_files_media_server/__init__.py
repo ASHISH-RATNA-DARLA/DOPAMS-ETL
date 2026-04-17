5a36b8267837adc4ff4a239e9bc98fa701916d7a
ashish-ratna-bccs <ashishratna@bluelcoudsoftech.com>
2026-04-17 12:05:38 +0530

    Remove __pycache__ from tracking - already in .gitignore

--- etl-files/etl_files_media_server/etl_files_media_server/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-ETL process for downloading files from the DOPAMAS files API
-and saving them into the Tomcat media server folder structure.
-
-This package contains the `etl_files_media_server` entrypoint script.
-"""
-
-
-
--- etl-files/etl_pipeline_files/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-ETL Pipeline for DOPAMAS Files Management
-Extracts file references from APIs and loads into files table
-"""
-
-__version__ = "1.0.0"
-
